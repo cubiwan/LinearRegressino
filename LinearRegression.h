cb9e3c3964e0b97d7a78d711437616e1ffa8591e
Cubiwan <cubiwan@gmail.com>
2018-11-07 09:47:29 +0100

    Update LinearRegression.h

--- LinearRegression.h
+++ LinearRegression.h
@@ -6,11 +6,11 @@
 class LinearRegression
 {
   public:
-    LinearRegression(int min, int max);
-    void learn(int x, int y);
-    double calculate(int x);
+    LinearRegression(double min, double max);
+    void learn(double x, double y);
+    double calculate(double x);
     double correlation();
-    void fixN(int maxN);
+    void fixN(double maxN);
     void getValues(double values[]);
   private:
     double meanX = 0;
@@ -21,10 +21,10 @@ class LinearRegression
     double varY = 0;
     double meanXY = 0; //mean x*y
     double covarXY = 0;
-    int n = 0;
+    double n = 0;
     bool fixedN = false;
-    int minX;
-    int maxX;
+    double minX;
+    double maxX;
     // m*x + b = y;
     double m = 0;
     double b = 0;
