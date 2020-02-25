84a1b5a9cd7a82103128d7bff4a7c01f46630458
Cubiwan <cubiwan@gmail.com>
2020-02-25 01:30:44 +0100

    Update LinearRegression.h

--- LinearRegression.h
+++ LinearRegression.h
@@ -3,32 +3,31 @@
 
 #include "Arduino.h"
 
-class LinearRegression
-{
-  public:
-    LinearRegression(double min, double max);
-    void learn(double x, double y);
-    double calculate(double x);
-    double correlation();
-    void fixN(double maxN);
-    void getValues(double values[]);
-  private:
-    double meanX = 0;
-    double meanX2 = 0; //mean x²
-    double varX = 0;
-    double meanY = 0;
-    double meanY2 = 0; //mean y²
-    double varY = 0;
-    double meanXY = 0; //mean x*y
-    double covarXY = 0;
-    double n = 0;
-    bool fixedN = false;
-    double minX;
-    double maxX;
-    // m*x + b = y;
-    double m = 0;
-    double b = 0;
-    double r = 0; //correlation
+class LinearRegression {
+    public:
+        LinearRegression();
+        LinearRegression(double min, double max);
+        void learn(double x, double y);
+        double calculate(double x);
+        double correlation();
+        void getValues(double values[]);
+        void reset();
+    private:
+        double meanX = 0;
+        double meanX2 = 0; //mean x²
+        double varX = 0;
+        double meanY = 0;
+        double meanY2 = 0; //mean y²
+        double varY = 0;
+        double meanXY = 0; //mean x*y
+        double covarXY = 0;
+        double n = 0;
+        boolean limits = false; 
+        double minX = 0;
+        double maxX = 0;
+        // m*x + b = y;
+        double m = 0;
+        double b = 0;
 };
 
 #endif
