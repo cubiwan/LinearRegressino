f57d4e00ac6dff78595c9215a89a835950361998
Cubiwan <cubiwan@gmail.com>
2020-02-25 01:27:43 +0100

    Update example.ino

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -1,33 +1,56 @@
 #include <LinearRegression.h>
 
-LinearRegression lr = LinearRegression(0,100);
+LinearRegression lr = LinearRegression();
+double values[3];
 
 void setup() {
-  lr.learn(1,3);
-  lr.learn(2,4);
-  lr.learn(3,5);
-  lr.learn(4,6);
-  lr.learn(5,7);
-  lr.learn(6,8);
+    lr.learn(1,3);
+    lr.learn(2,4);
+    lr.learn(3,5);
+    lr.learn(4,6);
+    lr.learn(5,7);
+    lr.learn(6,8);
 
-  Serial.begin(9600); 
-    
+    Serial.begin(9600);     
 }
 
 void loop() {
-  Serial.print("Result: ");
-  Serial.println(lr.calculate(6));
+    Serial.println("Start learn");
+    lr.learn(1,3);
+    lr.learn(2,4);
+    lr.learn(3,5);
+    lr.learn(4,6);
+    lr.learn(5,7);
+    lr.learn(6,8);
+    Serial.println("End learn");
 
-  Serial.print("Correlation: ");
-  Serial.println(lr.correlation());
-  
-  Serial.print("Values: ");
+    Serial.print("Result: 6 -> ");
+    Serial.println(lr.calculate(6));
 
-  lr.getValues(values);
-  Serial.print("Y = ");
-  Serial.print(values[0]);
-  Serial.print("*X + ");
-  Serial.println(values[1]);  
+    Serial.print("Result: 7 -> ");
+    Serial.println(lr.calculate(7));
 
-  delay(2000);
+    Serial.print("Correlation: ");
+    Serial.println(lr.correlation());
+
+    Serial.print("Values: ");
+    lr.getValues(values);
+    Serial.print("Y = ");
+    Serial.print(values[0]);
+    Serial.print("*X + ");
+    Serial.println(values[1]); 
+
+    Serial.println("Reset");
+    lr.reset();
+
+    Serial.print("Values: ");  
+    lr.getValues(values);
+    Serial.print("X = ");
+    Serial.print(values[0]);
+    Serial.print(" b = ");
+    Serial.print(values[1]); 
+    Serial.print(" n = ");
+    Serial.println(values[2]); 
+
+    delay(5000);
 }
